eeff1bb2781cf010271c376b48fd1d80c67f39ea
mortalfreeman <mortalfreeman6@gmail.com>
2026-09-09 11:03:20 +0400

    Update codegen_x64.h

--- src/codegen_x64.h
+++ src/codegen_x64.h
@@ -1,7 +1,9 @@
-#ifndef CLC_CODEGEN_X64_H
-#define CLC_CODEGEN_X64_H
-#include "ast.h"
+#ifndef CODEGEN_X64_H
+#define CODEGEN_X64_H
 
-int codegen_x64(Program*, const char*);
+#include "ast.h"
+#include "symtable.h"
+
+void codegen_x64_generate(ASTNode* program, SymTable* symtable, const char* out_asm_path);
 
 #endif
