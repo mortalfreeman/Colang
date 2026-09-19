6dde5923a123e278ffa2b48e6490d45c601f19ba
mortalfreeman <mortalfreeman6@gmail.com>
2026-09-20 03:44:27 +0400

    Delete codegen_x64.h

--- src/codegen_x64.h
+++
@@ -1,9 +0,0 @@
-#ifndef CODEGEN_X64_H
-#define CODEGEN_X64_H
-
-#include "ast.h"
-#include "symtable.h"
-
-void codegen_x64_generate(ASTNode* program, SymTable* symtable, const char* out_asm_path);
-
-#endif
